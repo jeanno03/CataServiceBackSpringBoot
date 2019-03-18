11faab4259a20c3933ce69a5b76d0721cafedd08
jeanno03 <jeannory.phou@gmail.com>
2019-03-18 23:43:51 +0100

    tuto finish

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,4 @@
 var app=angular.module("MyApp",["ui.router"]);
-// https://stackoverflow.com/questions/41169385/http-get-success-is-not-a-function
 
 app.config(function($stateProvider,$urlRouterProvider){
 $stateProvider.state('home',{
@@ -23,6 +22,7 @@ app.controller("HomeController", function(){
 
 });
 
+//https://stackoverflow.com/questions/41169385/http-get-success-is-not-a-function
 app.controller("MyController", function($scope,$http){
     $scope.pageProduits=null;
     $scope.motCle="";
@@ -55,16 +55,40 @@ app.controller("MyController", function($scope,$http){
     // .error(function(err){
     //     console.log(err);
     // })
-    }
-    
+    }  
     $scope.goToPage=function(p){
     $scope.pageCourante=p;
     $scope.chercherProduits();
 }
+});
+
+//https://stackoverflow.com/questions/36545765/error-success-is-not-a-function-in-angularjs
+app.controller("NewProduitController", function($scope,$http){
+
+$scope.produit={};
+$scope.mode=0;
+$scope.saveProduit=function(){
+
+	  $http.post("http://localhost:8080/produits", $scope.produit).then(function(response) {
+		  alert("success");
+		  data = response.data;
+		  $scope.produit=data; 
+		  $scope.mode=1;
+		  })
+//	$http.post("http://localhost:8080/produits",$scope.produit)
+//	.success(function(data){
+//		alert(data);		
+//	})
+//	.error(function(err){
+//		console.log(err);
+//	})
+};
+$scope.modeForm=function(){
+	$scope.mode=0;
+	  $scope.produit={designation:"", prix:0.0,quantite:0}; 
+}
 
 });
 
-app.controller("NewProduitController", function($){
 
-});
 
